6eef55d3060669959eddbb25e4422570ef37675d
Tomáš Duffek <tomasduffek14@gmail.com>
2023-02-17 15:13:23 +0100

    anotation in SoundPlayerManager.ts

--- src/modules/piano/services/SoundPlayerManager.ts
+++ src/modules/piano/services/SoundPlayerManager.ts
@@ -1,7 +1,14 @@
 import { Howl } from 'howler';
 import SoundSourceManager from '../services/SoundSourceManager';
 
+/**
+ * Service for sound manager
+ */
 class SoundPlayerManager {
+    /**
+     * Play tone
+     * @param name
+     */
     static play(name: string) {
         const sourceManager = new SoundSourceManager();
         let sound = new Howl({
